123f22e6f244c69196904e101b927ac16ea2891b
Erno Erdelyi <erno@componentsoft.io>
2024-03-28 16:13:45 +0100

    mercedes changes

--- mercedes/config.py
+++ mercedes/config.py
@@ -1,12 +1,1 @@
-from enum import IntEnum
-
-
-OPENAI_CHROMA_COLLECTION="chroma/openai"
-STRANSFORMERS_CHROMA_COLLECTION="chroma/stransformers"
-MISTRAL_CHROMA_COLLECTION="chroma/mistral"
-
-
-class EmbeddingType(IntEnum):
-    OPEN_AI = 0
-    SENTENCE_TRANSFORMER = 1
-    MISTRAL = 2
+../config.py
